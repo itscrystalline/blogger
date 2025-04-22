6ffe096dc1aa77f68e9966bbe654ea9716bf99bf
itscrystalline <pvpthadgaming@gmail.com>
2025-04-22 15:44:29 +0700

    added tarpaulin for code coverage

--- devenv.nix
+++ devenv.nix
@@ -5,9 +5,15 @@
   inputs,
   ...
 }: {
+  packages = [pkgs.cargo-tarpaulin];
+
   languages.rust = {
     enable = true;
     channel = "stable";
     components = ["rustc" "cargo" "clippy" "rustfmt" "rust-analyzer"];
   };
+
+  scripts.test-codecov.exec = ''
+    cargo tarpaulin --color always --skip-clean
+  '';
 }
